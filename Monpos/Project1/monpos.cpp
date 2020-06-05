d129bbfe0a55306171b73409790be219f1347bad
Stefan Misch <github@misel.net>
2020-06-05 22:39:38 +0200

    Read file and change monitors

--- Monpos/Project1/monpos.cpp
+++ Monpos/Project1/monpos.cpp
@@ -1,9 +1,14 @@
 #include <cassert>
 #include <Windows.h>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 #define MAX_NUM_MONITORS 10 //the regular windows desktop applet doesn't support more than 10 screens. So this is hardcoded.
+#define FILE_NAME "monitor_map.txt"
 
 LPRECT monitor_dimensions[10];
 
@@ -36,7 +41,11 @@ int DetectMonitors()
 void write_file() {
 
     int i = 0;
-    std::ofstream fout("afile.txt");
+    std::ofstream fout(FILE_NAME);
+
+    
+    fout << "# monpos generated monitor map" << std::endl;
+   
     while (monitor_dimensions[i] != NULL) {
         fout << i << "," << monitor_dimensions[i]->left << "," << monitor_dimensions[i]->top << std::endl;
         i++;
@@ -48,14 +57,65 @@ void write_file() {
 int main() {
     int monitor_count;
 
-    monitor_count = DetectMonitors();
-    if (monitor_count == -1) {
-        std::cout << "Could not detect monitors" << std::endl;
-        return -1;
+    std::ifstream map_file;
+    map_file.open(FILE_NAME);
+
+    //if no file is found detect monitors and write a new file
+    if (map_file.fail()) {
+        monitor_count = DetectMonitors();
+        if (monitor_count == -1) {
+            std::cout << "Could not detect monitors" << std::endl;
+            return 1;
+        }
+        write_file();
+        return 0;
     }
 
+    //read file line by line
+    std::string line;
+    while (std::getline(map_file, line))
+    {
+        if (line[0] == '#') {
+            continue;
+        }
+        std::vector<int> vect;
+        std::cout << line << std::endl;
+
+        std::stringstream ss(line);
+
+        for (int i; ss >> i;) {
+            vect.push_back(i);
+            if (ss.peek() == ',')
+                ss.ignore();
+        }
+
+        //if vect doesn't contain 3 ints, skip
+        //(basically "ignore empty lines")
+        if (vect.size() != 3) {
+            continue;
+        }
+        
+        DEVMODEW mode{ {}, {}, {}, sizeof(DEVMODEW), 0 };
+
+        mode.dmFields = DM_POSITION;
+        mode.dmPosition.x = vect[1];
+        mode.dmPosition.y = vect[2];
+
+        DISPLAY_DEVICEW info{ sizeof(DISPLAY_DEVICEW) };
+
+        //load current Display settings
+        auto const result0 = EnumDisplayDevicesW(NULL, vect[0], &info, EDD_GET_DEVICE_INTERFACE_NAME);
+        if (result0 == 0) {
+            return 2;
+        }
+
+        //change current display
+        auto const result1 = ChangeDisplaySettingsExW(info.DeviceName, &mode, NULL, CDS_GLOBAL | CDS_UPDATEREGISTRY, NULL);
+        if (result1 != DISP_CHANGE_SUCCESSFUL) {
+            return 3;
+        }
+    }
 	
-    write_file();
     return 0;
 }
 
